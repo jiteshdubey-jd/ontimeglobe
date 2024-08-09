6122d020800c302443115b0cfd819ca6fcf5f2e0
Aspireve <steve1818fernandes@gmail.com>
2024-08-09 17:30:59 +0530

    navbar responsive with css

--- src/components/static/Navbar.jsx
+++ src/components/static/Navbar.jsx
@@ -1,16 +1,17 @@
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import contactDetails from "@/data/contactDetails";
+import "../styles/navbar.css"
 
 export const Navbar = () => {
   return (
-    <div className="pt-2 flex justify-between pb-2 w-4/5 mx-auto">
-      <div className="flex justify-center items-center space-x-4">
+    <div className="navbar">
+      <div className="contact-info">
         <p>{contactDetails.email}</p>
-        <Separator className="w-[3px]" orientation="vertical"/>
-        <p>{contactDetails.phoneNumber}</p>
+        <Separator className="separator hide-when-small" orientation="vertical" />
+        <p className="hide-when-small">{contactDetails.phoneNumber}</p>
       </div>
-      <Button className="bg-[#EC4621] hover:bg-[#FF6A35]">GET QUOTE</Button>
+      <Button className="custom-btn">GET QUOTE</Button>
     </div>
   );
 };
